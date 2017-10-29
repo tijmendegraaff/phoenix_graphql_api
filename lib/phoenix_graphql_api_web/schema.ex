6d2d9f730c124abd8571d0c49a3c3874bf7437f1
Tijmen de Graaff <tijmen76@gmail.com>
2017-10-30 00:00:17 +0100

    plug using Guardian

--- lib/phoenix_graphql_api_web/schema.ex
+++ lib/phoenix_graphql_api_web/schema.ex
@@ -35,7 +35,20 @@ defmodule PhoenixGraphqlApiWeb.Schema do
         field :user_id, non_null(:integer)
     end
 
+    input_object :update_user_params do
+        field :username, :string
+        field :email, :string
+        field :password, :string
+    end
+
     mutation do
+        field :update_user, type: :user do
+            arg :id, non_null(:integer)
+            arg :user, :update_user_params
+         
+            resolve &UserResolver.update/2
+        end
+
         field :create_post, type: :post do
             arg :title, non_null(:string)
             arg :body, non_null(:string)
