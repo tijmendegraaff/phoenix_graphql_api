1808721c73e102bed4f934822d1d4c1b90c38995
Tijmen de Graaff <tijmen76@gmail.com>
2017-10-29 20:35:20 +0100

    create seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,12 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     PhoenixGraphqlApi.Repo.insert!(%PhoenixGraphqlApi.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias PhoenixGraphqlApi.{Post, Repo, User}
+
+Repo.insert!(%User{username: "Ryan Swapp", email: "ryan@ryan.com"})
+Repo.insert!(%User{username: "Rosie", email: "rosie@mydog.com"})
+
+for _ <- 1..10 do
+    Repo.insert!(%Post{
+        title: Faker.Lorem.sentence,
+        body: Faker.Lorem.paragraph,
+        user_id: Enum.random(1..2)
+    })
+end
