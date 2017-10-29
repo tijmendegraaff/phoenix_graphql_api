9435f80f313c801a5981bfd7a20fb67524c1577e
Tijmen de Graaff <tijmen76@gmail.com>
2017-10-29 22:10:33 +0100

    add Post CRUD mutations

--- lib/phoenix_graphql_api_web/resolvers/post_resolver.ex
+++ lib/phoenix_graphql_api_web/resolvers/post_resolver.ex
@@ -13,4 +13,20 @@ defmodule PhoenixGraphqlApiWeb.PostResolver do
         end
     end
     
+    def create(args, _info) do
+        %Post{}
+        |> Post.changeset(args)
+        |> Repo.insert
+    end
+
+    def update(%{id: id, post: post_params}, _info) do
+        Repo.get!(Post, id)
+        |> Post.changeset(post_params)
+        |> Repo.update
+    end
+
+    def delete(%{id: id}, _info) do
+        post = Repo.get!(Post, id)
+        Repo.delete(post)
+    end
 end
--- lib/phoenix_graphql_api_web/schema.ex
+++ lib/phoenix_graphql_api_web/schema.ex
@@ -29,6 +29,12 @@ defmodule PhoenixGraphqlApiWeb.Schema do
         end
     end
 
+    input_object :update_post_params do
+        field :title, non_null(:string)
+        field :body, non_null(:string)
+        field :user_id, non_null(:integer)
+    end
+
     mutation do
         field :create_post, type: :post do
             arg :title, non_null(:string)
@@ -37,5 +43,19 @@ defmodule PhoenixGraphqlApiWeb.Schema do
             
             resolve &PostResolver.create/2
         end
+
+        field :update_post, type: :post do
+            arg :id, non_null(:integer)
+            arg :post, :update_post_params
+         
+            resolve &PostResolver.update/2
+        end
+
+        field :delete_post, type: :post do
+            arg :id, non_null(:integer)
+         
+            resolve &PostResolver.delete/2
+        end
     end
+
 end
